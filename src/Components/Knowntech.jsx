edd150b736100f7251e9820364a6ddd109749dfd
Foparty <103907142+Foparty@users.noreply.github.com>
2022-12-28 22:01:34 +0000

    section 2 done

--- src/Components/Knowntech.jsx
+++ src/Components/Knowntech.jsx
@@ -56,8 +56,43 @@ const Knowntech = () => {
               widthpercentage={70}
               progressbg="bg-tailwind"
             />
+            <TechIconPill
+              classes="icon-pill span-2"
+              icoclasses="icon-ico"
+              faicon="fa-brands fa-mdb"
+              widthpercentage={80}
+              progressbg="bg-mdb"
+            />
+            <TechIconPill
+              classes="icon-pill span-3"
+              icoclasses="icon-ico"
+              faicon="fa-brands fa-github"
+              widthpercentage={50}
+              progressbg="bg-github"
+            />
+            <TechIconPill
+              classes="icon-pill span-3"
+              icoclasses="icon-ico"
+              faicon="fa-brands fa-node-js"
+              widthpercentage={20}
+              progressbg="bg-node"
+            />
+            <TechIconPill
+              classes="icon-pill span-6 rounded-bottom"
+              icoclasses="icon-ico"
+              faicon="fa-brands fa-npm"
+              widthpercentage={80}
+              progressbg="bg-npm"
+            />
           </ul>
-          <div className="content-tech">hola</div>
+          <div className="content-tech">
+            <h2 className="title">myStack( );</h2>
+            <p className="code">
+              Improving and adding new technologies to my Stack every day. Contact me and let me
+              help you or you business take the next step!
+            </p>
+            <p className="code">Logic games and fixing bugs ❤️ = exercise for the brain 🧠</p>
+          </div>
         </div>
       </div>
     </section>
