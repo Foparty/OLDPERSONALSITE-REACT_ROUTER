4acd012229e1434b8e784cd1259f01aaa5e89ea6
Foparty <103907142+Foparty@users.noreply.github.com>
2022-12-29 10:30:25 +0000

    image update

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,6 @@ export { default as hero1 } from './hero1.jpg';
 export { default as hero2 } from './hero2.jpg';
 export { default as hero3 } from './hero3.jpg';
 export { default as hero4 } from './hero4.jpg';
+export { default as hero5 } from './hero5.jpg';
 export { default as paralax1 } from './paralax1.jpg';
 export { default as paralax2 } from './paralax2.jpg';
