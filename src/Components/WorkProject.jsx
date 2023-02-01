b96edfcabfe4a44c46542177717fd9e5c785a001
Foparty <103907142+Foparty@users.noreply.github.com>
2023-02-01 08:31:54 +0100

    work update

--- src/Components/WorkProject.jsx
+++ src/Components/WorkProject.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import './WorkProject.css';
-import { gabriel } from '../assets';
 
-const WorkProject = () => {
+const WorkProject = ({ url, img, title, description }) => {
   return (
     <article className="project">
-      <a
-        href="https://gabrielitoweb.netlify.app/"
-        target="_blank"
-        rel="noreferrer"
-        className="project-card"
-      >
-        <img className="project-avatar" src={gabriel} alt="" />
+      <a href={url} target="_blank" rel="noreferrer" className="project-card">
+        <img className="project-avatar" src={img} alt="" />
       </a>
       <aside className="project-info">
-        <a href="https://gabrielitoweb.netlify.app/" target="_blank" rel="noreferrer">
-          <h3 className="project-title">Gabriel Asdrúbal | Fotografía y Arquitectura</h3>
+        <a href={url} target="_blank" rel="noreferrer">
+          <h3 className="project-title">{title}</h3>
         </a>
         <h4>Project Description:</h4>
-        <p>
-          Interesting React project in witch I had to overcome problems with grid layout, and help
-          me come up with interesting concepts for it and future projects. As well as fixing text
-          resize for better responsive sites.
-        </p>
+        <p>{description}</p>
       </aside>
     </article>
   );
--- src/Containers/Workpreview.jsx
+++ src/Containers/Workpreview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import WorkProject from '../Components/WorkProject';
 import './Workpreview.css';
+import { gabriel } from '../assets';
 
 const Workpreview = () => {
   return (
@@ -9,7 +10,14 @@ const Workpreview = () => {
       {/* <p>
         in progress...<span className="emoji">👷‍♂️⚠️</span>
       </p> */}
-      <WorkProject />
+      <WorkProject
+        url="https://gabrielitoweb.netlify.app/"
+        img={gabriel}
+        title="Gabriel Asdrúbal | Fotografía y Arquitectura"
+        description="Interesting React project in witch I had to overcome problems with grid layout, and help
+          me come up with interesting concepts for it and future projects. As well as fixing text
+          resize for better responsive sites."
+      />
     </section>
   );
 };
