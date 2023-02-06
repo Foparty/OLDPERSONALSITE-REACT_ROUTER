b53215882b62ef01641b3c99fbed973c1c0926c7
Foparty <103907142+Foparty@users.noreply.github.com>
2023-02-06 16:29:21 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar';
-import { Home, Blog, Fem } from './Containers';
+import { Home, Blog, Fem, Cv } from './Containers';
 
 function App() {
   return (
@@ -10,8 +10,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blog" element={<Blog />} />
+        <Route path="/cv" element={<Cv />} />
         <Route path="/fem" element={<Fem />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
